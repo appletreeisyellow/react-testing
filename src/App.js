6c3d08e267479ed3eb79e17726f2e3eeb56217e1
Chunchun <yccvivo@gmail.com>
2022-03-22 15:19:25 -0500

    feat: add logic and test for red and blue button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const BLUE = 'blue';
+const RED = 'red';
 
 function App() {
+  const [currentColor, setCurrentColor] = useState(RED)
+
   return (
     <div>
-      <button style={{backgroundColor: 'red'}}>
-        Change to blue
+      <button style={{backgroundColor: currentColor}}
+      onClick={() => setCurrentColor(currentColor === BLUE ? RED : BLUE)}>
+        Change to {currentColor === BLUE ? RED : BLUE}
       </button>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial text and color', () => {
   render(<App/>);
 
   // find an element with a role of button and text of 'Change of blue
-  const colorButton = screen.getByRole('button', { name: 'Change to blue'})
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'});
 
   // expect the background color to be red
-  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
-});
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
 
-test('button turns blue when clicked', () => {
-  
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' })
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe('Change to red')
 });
